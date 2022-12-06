5ff892ddc6a8537b8d5a233ff09fdcb462ccd580
Akinyele Ibrahim <iakinyele3@gmail.com>
2022-12-06 22:49:10 +0100

    Assignment Completed

--- assignments/lib/main.dart
+++ assignments/lib/main.dart
@@ -1,4 +1,28 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(AssignmentOne());
 
+class AssignmentOne extends StatelessWidget {
+  int click = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter"),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text("My First Flutter app"),
+        ),
+        floatingActionButton:
+            FloatingActionButton(onPressed: clicked, child: const Text("Hi")),
+      ),
+    );
+  }
 
+  void clicked() {
+    click++;
+    print('Button Clicked... ${click} times');
+  }
+}
